fa342a8a19cb590292c94a518053cf7f4ce2dabf
Sanidhya Vijaywat <vijaywatsanidhya@gmail.com>
2018-01-31 21:01:26 +0530

    Added newton raphson method.....however i have to test it with some more test case functions

--- test_bench.py
+++ test_bench.py
@@ -3,13 +3,13 @@ import numpy
 import math
 import time
 import matplotlib.pyplot as plt
-a = -50
-b = 100
+a = -2
+b = 3
 epsilon = 0.01
 width = 0.01
 x_vector = numpy.arange(a,b,width)
 def fn(x):
-    return(math.pow(x,3) - math.pow(x,2) + 4)
+    return(math.pow(x,2))
 t1 = time.clock()
 root1 = univarroot.bisection_method(fn,a,b,epsilon,width)
 print(root1)
@@ -17,9 +17,21 @@ t2 = time.clock()
 root2 = univarroot.exhaustive(fn,x_vector)
 print(root2)
 t3 = time.clock()
+xo =2
+while (1):
+    xn = xo - ((univarroot.d1_fn(fn, xo, width)) / univarroot.d2_fn(fn, xo, width))
+    print(xn)
+    xo = xn
+    root3 = xo
+    if(abs(fn(xo))<epsilon):
+        break
+print(xo)
+print(root3)
+t4 = time.clock()
 print(t2-t1)
 print(t3-t2)
+print(t4-t3)
 fnv = numpy.vectorize(fn)
-plt.plot(fnv(x_vector),x_vector)
+plt.plot(x_vector,fnv(x_vector))
 plt.grid(1)
 plt.show()
